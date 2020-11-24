751a7300b94cd49bbf68c2bc6f9db1b36efdd0f2
durgasi777 <59000045+durgasi777@users.noreply.github.com>
2020-11-24 11:07:26 +1100

    Update terraform.tf

--- Terraform/Dev/terraform.tf
+++ Terraform/Dev/terraform.tf
@@ -2,7 +2,5 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraformstate"
     storage_account_name = "krishterrastatestorage"
-    container_name       = "terraformdemo"
-    key                  = "terraform.tfstate"
   }
 }
